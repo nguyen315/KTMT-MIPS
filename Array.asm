9a2e85b07ca13b4144986ace854ec4ea67ddb583
dotaallstars200 <dragonallstar270520@gmail.com>
2020-06-13 19:30:01 +0700

    Update Array.asm
    Finish ListPrimeArray

--- Array.asm
+++ Array.asm
@@ -5,6 +5,7 @@ n: .word 0 # SL phan tu
 strNhapN: .asciiz "Nhap so luong phan tu n(n>0): "
 strNhapPhanTu: .asciiz "array["
 strNhapPhanTu_: .asciiz "] = "
+strDauPhay: .asciiz ", "
 
 
 .text
@@ -70,8 +71,77 @@ NhapMangNPhanTu:
 
 ############# Ket thuc phan nhap mang
 
+# Main
+jal LietKeSNT
+j FinishProcedure
 
+############# Liet ke phan tu la so nguyen to
+
+#### Ham Chinh
+LietKeSNT:
+	# Khoi tao bien dem o $t0
+	li $t0, 0
+	la $t5, array
+	move $t6, $ra
+
+	# --- Duyet tung phan tu trong mang
+	Loop:
+		# Lay gia tri array[$t0] luu vao $t1
+		
+		lw $t1, ($t5)
+		jal isPrime
+		
+		# Kiem tra gia tri tra ve o $v0
+		beq $v0, $zero, SetUpNextLoop
+		# Neu bang 1 thi in gia tri SNT		
+		move $a0, $t1
+		li $v0, 1
+		syscall
+
+		la $a0, strDauPhay
+		li $v0, 4
+		syscall
+
+		SetUpNextLoop:
+			# Tang bien dem
+			addi $t0, $t0, 1
+			addi $t5, $t5, 4 # $t5 luu gia tri dia chi phan tu array hien tai
+			# So sanh dieu kien dung vong lap
+			blt $t0, $s2, Loop
+
+	# ket thuc ham
+	move $ra, $t6
+	jr $ra
+
+# ---------- Ket thuc ham chinh
+
+#### Kiem tra mot so la so nguyen to
+isPrime:
+	blt $t1, 2, isPrimeFalse # t1 < 2
+	beq $t1, 2, isPrimeTrue # t1 == 2
+
+	# t1 > 2 
+	# Khoi tao bien dem o $t2
+	li $t2, 2
+	IsPrimeLoop:
+		div $t3, $t1, $t2       # $t3 = $t1 / $t2
+		mfhi $t4                # $t4 = so du
+		beq $t4, $zero, isPrimeFalse
+		# Tang bien dem
+		addi $t2, $t2, 1
+		# So sanh dieu kien dung vong lap
+		blt $t2, $t1, IsPrimeLoop
+
+	isPrimeTrue:
+		li $v0, 1
+		jr $ra
+	isPrimeFalse:	
+		li $v0, 0
+		jr $ra
+
+############# Ket thucc liet ke phan tu la so nguyen to
 
 # Ket thuc chuong trinh
+FinishProcedure:
 li $v0, 10
 syscall
